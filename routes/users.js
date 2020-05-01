78e61ac69f6c1d018b0208b42d840968bed2f69b
Janio Isacura <janioisacura@gmail.com>
2020-04-30 21:02:41 -0300

    Pruebas importando modelo en controlador

--- routes/users.js
+++ routes/users.js
@@ -1,30 +1,17 @@
 let fs = require('fs');
 let express = require('express');
 let router = express.Router();
-
-let request = require("request");
-
-let options = {
-  method: 'GET',
-  url: 'https://rawg-video-games-database.p.rapidapi.com/games',
-  headers: {
-    'x-rapidapi-host': 'rawg-video-games-database.p.rapidapi.com',
-    'x-rapidapi-key': '1af368a0admshbaf56e0791f798cp1f836bjsnc1ced84aa968'
-  }
-};
+let JuegosModel = require('../models/juegosModel');
 
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  request(options, function (error, response, body) {
-    let data = JSON.parse(body);
-    
-    res.render('users', {
-      nombre: 'Homero',
-      apellido: 'Thompson',
-      title: 'GAME List',
-      gameList: data
-    });
+router.get('/', function (req, res, next) {
+  console.log(JuegosModel);
+  
+  res.render('users', {
+    nombre: 'Homero',
+    apellido: 'Thompson',
+    title: 'GAME List',
+    gameList: JuegosModel
   });
 });
 
