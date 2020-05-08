9aa6caec00749cd67642789643479cd0e8cfca91
Janio Isacura <janioisacura@gmail.com>
2020-05-08 17:27:45 -0300

    Se agregó pagina de error 404

--- app.js
+++ app.js
@@ -66,13 +66,19 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  if (res.status(404)) {
+    res.status(400).render('notfound');
+  } else {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  }
+  
+  
 });
 
 module.exports = app;
