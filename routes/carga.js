a020dea1c3e1d3a18b44c200df5d4a7e46a004a7
Janio Isacura <janioisacura@gmail.com>
2020-05-10 21:05:16 -0300

    Actualización estilos carga producto

--- routes/carga.js
+++ routes/carga.js
@@ -10,11 +10,22 @@ router.get('/', function (req, res, next) {
   request(JuegosModel, function (error, response, body) {
     let data = JSON.parse(body);
     if (error) throw new Error(error);
+    let idJuego = req.params.id;
+    let gameList = data;
+    let getRatings = gameList.results[0].ratings;
+
+    // First, get the max vote from the array of objects
+    var maxVotes = Math.max(...getRatings.map(e => e.percent));
+
+    // Get the object having votes as max votes
+    var maxRated = getRatings.find(game => game.percent === maxVotes);
       res.render('carga', {
         nombre: 'Admin',
         apellido: '-',
         title: 'carga',
+        juego: gameList.results[0],
         gameList: data,
+        rating: maxRated.title
       });
   });
 });
