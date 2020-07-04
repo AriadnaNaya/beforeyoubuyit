de6edf3d1f1bd7c85f1f23942b2dda139f055ec1
Janio Isacura <janioisacura@gmail.com>
2020-07-04 20:38:44 -0300

    Actualización de scripts

--- scripts/selectProductosPorTtienda.sql
+++ scripts/selectProductosPorTtienda.sql
@@ -2,4 +2,5 @@ select *
 from products
 inner join products_stores on products.id = products_id
 inner join stores on stores.id = stores_id
+inner join categories on categories.id = categories_id
 order by products.name asc
